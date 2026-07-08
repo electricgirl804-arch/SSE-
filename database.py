402db0df5d0ae3f069c9682abeac54540752bc1a
Electrical Engineer (Control) <electricgirl804@gmail.com>
2026-07-08 14:16:26 +0200

    Delete database.py

--- database.py
+++
@@ -1,8 +0,0 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-
-def connect_sheet():
-    scope = ["https://spreadsheets.google.com/feeds"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("sse-credentials.json", scope)
-    client = gspread.authorize(creds)
-    return client.open("SSE_Customers").sheet1
